96b58051edd9567e05d185ac44783f85b84e89b2
justinxyu1128 <justinxyu1128@gmail.com>
2025-03-04 00:37:03 -0800

    added bootstrap navigation and styling

---
+++ public/js/load.js
@@ -0,0 +1,5 @@
+function loadNavigation(){   
+    console.log($('#navbarPlaceholder').load('/text/header.html'));
+    console.log($('#footerPlaceholder').load('/text/footer.html'));           
+}
+loadNavigation(); 
