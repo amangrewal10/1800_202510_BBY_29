f7407da1cf6f8dd9676a76e98f7f770857fa94ab
justinxyu1128 <justinxyu1128@gmail.com>
2025-03-08 22:30:34 -0800

    added html for workshops, upcoming workshops pages

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use("/text", express.static("./public/text"));
 app.use("/fonts", express.static("./public/css/fonts"));
 app.use("/home", express.static("./app/html/index.html"));
 app.use("/login", express.static("./app/html/login.html"));
+app.use("/workshops", express.static("./app/html/workshops.html"));
 app.use("/setup-form", express.static("./app/html/setup_form.html"));
+app.use("/upcoming", express.static("./app/html/upcoming_workshops.html"));
 
 app.get("/", function(req, res) {
     //console.log(process.env);
